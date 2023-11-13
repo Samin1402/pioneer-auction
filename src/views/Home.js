357698f085cd6bfb03b09996e68d0d7d30c0b7f2
Samin0710 <samin99asif@gmail.com>
2023-11-13 12:47:39 +0500

    Updated

--- src/views/Home.js
+++ src/views/Home.js
@@ -16,69 +16,33 @@ const Home = () => {
   return (
     <div>
       <Card>
-        <CardHeader>
-          <CardTitle>Home</CardTitle>
-        </CardHeader>
         <CardBody>
-         <main className='main-container'>
-      
-
+        
         <div className='main-cards'>
             <div className='card'>
                 <div className='card-inner'>
                 <img className="card_icon" src={chats} alt="Login Cover" />
                     <h1>500</h1>
-                    <h3>All Chats</h3>
-                </div>
-            </div>
-            <div className='card' >
-                <div className='card-inner'>
-                <img className="card_icon" src={leads} alt="Login Cover" />
-                    
-                    <h1>100</h1>
-                    <h3>Leads</h3>
-                </div>
-            </div>
-            <div className='card'>
-                <div className='card-inner' >
-                <img className="card_icon" src={support} alt="Login Cover" />
-                    
-                    <h1>100</h1>
-                    <h3>Support Requests</h3>
+                    <CardTitle>All Chats</CardTitle>
                 </div>
             </div>
         </div>
-        <div className='main-cards'>
-        <div className='card2'>
-                <div className='card-inner' >
-                <img className="card_icon" src={refund} alt="Login Cover" />
-                    
-                    <h1>100</h1>
-                    <h3>Refund Requests</h3>
-                </div>
-            </div>
-            <div className='card2'>
-                <div className='card-inner' >
-                <img className="card_icon" src={mappin1} alt="Login Cover" />
-                    
-                    <h1>100</h1>
-                    <h3>Locations</h3>
-                </div>
-            </div>
-            <div className='card2'>
-                <div className='card-inner' >
-                <img className="card_icon" src={complaint} alt="Login Cover" />
-                    
-                    <h1>100</h1>
-                    <h3>Complaints</h3>
-                </div>
-            </div>
-        </div>
-    </main>
         </CardBody>
       </Card>
-
-     
+      <Card>
+        <CardBody>
+        
+        <div className='main-cards'>
+            <div className='card'>
+                <div className='card-inner'>
+                <img className="card_icon" src={chats} alt="Login Cover" />
+                    <h1>500</h1>
+                    <CardTitle>Leads</CardTitle>
+                </div>
+            </div>
+        </div>
+        </CardBody>
+      </Card>
     </div>
   );
 };
